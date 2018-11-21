329c5b41b4e365367c7e264c32c862671d09f08e
Ayush Gupta <35647598+Ayush6@users.noreply.github.com>
2018-11-21 11:58:14 +0530

    Weighted_Mean.py

--- Weighted Median.py
+++ Weighted Median.py
@@ -1,10 +1,11 @@
 n = int(input())
 
 upper = 0
-
+#Array Input
 lst1 = list(map(int, input().split()))
 lst2 = list(map(int, input().split()))
+
 for i in range(n):
     upper += lst1[i] * lst2[i]
-
-print(upper/sum(lst2))
+    
+print("{:.1f}".format(upper/sum(lst2)))
